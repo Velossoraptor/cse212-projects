0c644fb5e7ba6c7417059fe4dd8ecd7585c9a37e
velossoraptor <taliamolsen@gmail.com>
2025-07-08 10:13:34 -0400

    Priority Queue Test Cases finished

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,88 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Item with high priority is added at back of queue
+    // Expected Result: lemon, banana, apple, orange
+    // Defect(s) Found: starts at wrong index, stops to early, not removing after Dequeue
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var apple = new PriorityItem("apple", 1);
+        var banana = new PriorityItem("banana", 2);
+        var orange = new PriorityItem("orange", 0);
+        var lemon = new PriorityItem("lemon", 4);
+
+        priorityQueue.Enqueue(apple.Value, apple.Priority);
+        priorityQueue.Enqueue(banana.Value, banana.Priority);
+        priorityQueue.Enqueue(orange.Value, orange.Priority);
+        priorityQueue.Enqueue(lemon.Value, lemon.Priority);
+
+        PriorityItem[] expected = [lemon, banana, apple, orange];
+        var item = priorityQueue.Dequeue();
+        Assert.AreEqual(expected[0].Value, item);
+        item = priorityQueue.Dequeue();
+        Assert.AreEqual(expected[1].Value, item);
+        item = priorityQueue.Dequeue();
+        Assert.AreEqual(expected[2].Value, item);
+        item = priorityQueue.Dequeue();
+        Assert.AreEqual(expected[3].Value, item);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Items with tie 
+    // Expected Result: banana, lemon, apple, orange
+    // Defect(s) Found: was checking if equal to, should only check if greater than priority
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var apple = new PriorityItem("apple", 1);
+        var banana = new PriorityItem("banana", 2);
+        var orange = new PriorityItem("orange", 0);
+        var lemon = new PriorityItem("lemon", 2);
+
+        priorityQueue.Enqueue(apple.Value, apple.Priority);
+        priorityQueue.Enqueue(banana.Value, banana.Priority);
+        priorityQueue.Enqueue(orange.Value, orange.Priority);
+        priorityQueue.Enqueue(lemon.Value, lemon.Priority);
+
+        PriorityItem[] expected = [banana, lemon, apple, orange];
+        var item = priorityQueue.Dequeue();
+        Assert.AreEqual(expected[0].Value, item);
+        item = priorityQueue.Dequeue();
+        Assert.AreEqual(expected[1].Value, item);
+        item = priorityQueue.Dequeue();
+        Assert.AreEqual(expected[2].Value, item);
+        item = priorityQueue.Dequeue();
+        Assert.AreEqual(expected[3].Value, item);
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Empty Queue
+    // Expected Result: error
+    // Defect(s) Found: 
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Error should have been thrown");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+        catch (AssertFailedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(
+                 string.Format("Unexpected exception of type {0} caught: {1}",
+                                e.GetType(), e.Message)
+            );
+        }
+    }
 }
